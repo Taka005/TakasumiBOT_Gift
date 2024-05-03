78b1efcde340fbf857169643d4737eace988d08f
Taka005 <01taka005@gmail.com>
2024-05-03 11:04:01 +0900

    fix

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $res = json_decode(file_get_contents("https://api.takasumibot.com/v1/gift?id=".h
 
 $data = $res["data"];
 
-if(!$data) return header("Location: ./invalid.html");
+if(!$data) return require_once __DIR__."/invalid.php";
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -24,7 +24,7 @@ if(!$data) return header("Location: ./invalid.html");
         <meta property="og:type" content="website" />
         <meta property="og:title" content="ギフトが送られてきました" />
         <meta property="og:description" content="コマンドで入力してゲットしよう" />
-        <meta property="og:site_name" content="TakasumiBOT Servers" />
+        <meta property="og:site_name" content="TakasumiBOT Gift" />
         <meta property="og:image" content="/assets/img/takasumibot.png" />
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
